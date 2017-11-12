fc00f9c84decf4e68a9b97dcec82b9e512e1c76a
Alberto <alberto@migasfree.org>
2017-11-12 18:29:03 +0100

    added debian package

---
+++ deb/make-deb.sh
@@ -0,0 +1,19 @@
+dpkg -l|grep 'ii  npm ' > /dev/null
+if [ $? != 0 ]
+then
+  echo "please, install npm: apt-get install npm" 
+  exit
+fi
+
+mkdir -p usr/share/migasfree-play
+cp -r ../src/* usr/share/migasfree-play/
+cd usr/share/migasfree-play/
+npm install
+cd -
+/usr/bin/debuild --no-tgz-check -us -uc
+mkdir -p ../dist
+mv ../migasfree*.deb ../dist
+rm ../migasfree-play*.dsc
+rm ../migasfree-play*.tar.xz
+rm ../migasfree-play*.build
+rm -rf ../migasfree-play*.changes
