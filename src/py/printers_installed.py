1c3bbfa7aa25bf2506552cf49086b88d0f3f3f3b
Gacias Mateo Alberto <od040w@PC25619.red.zaragoza.es>
2017-11-20 13:31:42 +0100

    Bugfix: printer installed in Windows

--- src/py/printers_installed.py
+++ src/py/printers_installed.py
@@ -1,19 +1,34 @@
 from __future__ import print_function
-import cups
 
-try:
-    conn = cups.Connection()
-except RuntimeError:
-    conn = None
+import platform
 
-installed = []
-if conn:  # cups is running
-    printers = conn.getPrinters()
+OS = platform.system()
 
-    for printer in printers:
-        p=printers[printer]
-        if len(p['printer-info'].split('__')) == 5:
-            installed.append(int(p['printer-info'].split('__')[4]))
+if OS == "Linux":
+    import cups
+    try:
+        conn = cups.Connection()
+    except RuntimeError:
+        conn = None
 
-print(installed,end='')
+    installed = []
+    if conn:  # cups is running
+        printers = conn.getPrinters()
+
+        for printer in printers:
+            p = printers[printer]
+            if len(p['printer-info'].split('__')) == 5:
+                installed.append(int(p['printer-info'].split('__')[4]))
+
+    print(installed, end='')
+elif OS == "Windows":
+    from migasfree_client.devices import LocalDevice
+
+    installed = []
+    local_devices = LocalDevice()
+
+    for key in local_devices.printers:
+        installed.append(key)
+
+    print(installed, end='')
 
