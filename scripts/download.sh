b7c71719dfd753b60c4814b3fae7c5e15d9cd707
Alberto Gacías <alberto@migasfree.org>
2018-07-16 10:54:39 +0200

    npm not strict-ssl

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,9 +1,9 @@
-set -e 
+set -e
 
 PATH=$PATH:/usr/local/bin
 
 if [ "$DEB_BUILD_ARCH_BITS" = "32" ]; then
-	_ARCH="ia32" 
+	_ARCH="ia32"
 else
 	_ARCH="x64"
 fi
@@ -11,6 +11,7 @@ _FILENAME=nwjs-v0.31.2-linux-$_ARCH
 
 
 cd usr/share/migasfree-play
+npm set strict-ssl false
 npm install
 
 wget -O nwjs.tar.gz https://dl.nwjs.io/v0.31.2/$_FILENAME.tar.gz
