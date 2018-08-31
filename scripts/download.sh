3ba8e2acd69fdb043f66e800ae397cb8f50d708f
Alberto Gacías <alberto@migasfree.org>
2018-08-31 22:42:54 +0200

    Added quiet parameter to wget

--- scripts/download.sh
+++ scripts/download.sh
@@ -5,13 +5,13 @@ _NWJS_VERSION=0.31.5
 PATH=$PATH:/usr/local/bin
 
 if [ "$DEB_BUILD_ARCH_BITS" = "32" ]; then
-	_ARCH="ia32"
+    _ARCH="ia32"
 else
-	_ARCH="x64"
+    _ARCH="x64"
 fi
 
 cd usr/share/migasfree-play
 npm set strict-ssl false
 npm install
 
-wget --tries=3 --no-check-certificate -O nwjs.tar.gz https://dl.nwjs.io/v${_NWJS_VERSION}/nwjs-v${_NWJS_VERSION}-linux-${_ARCH}.tar.gz
+wget --quiet --tries=3 --no-check-certificate -O nwjs.tar.gz https://dl.nwjs.io/v${_NWJS_VERSION}/nwjs-v${_NWJS_VERSION}-linux-${_ARCH}.tar.gz
